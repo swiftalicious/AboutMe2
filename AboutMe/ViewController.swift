bc401aee10f5b991437dcee879720621a1e7a9a5
swiftalicious <amazinggrace1@protonmail.com>
2019-05-09 18:48:42 -0700

    Remove print statement

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("My name is Jason")
+//        print("My name is Jason")
     }
 
 
